9c8c41fe36a17447de28624a872a9dbc4001f8cf
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-30 14:36:13 +0100

    inserisci rotta show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,4 +11,22 @@ function index(req, res) {
     });
 }
 
-module.exports = { index }
+function show(req, res) {
+    
+    const id = req.params.id
+    console.log("ID richiesto:", id); // 👈 Aggiungi questo
+    const sql = `
+        SELECT *
+        FROM movies
+        JOIN reviews
+        ON reviews.movie_id = movies.id
+        WHERE movies.id = ?
+    `;
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.length === 0) return res.status(404).json({ error: 'Film not found' })
+        res.json(results[0]);
+    });
+}
+
+module.exports = { index, show }
--- routers/movies.js
+++ routers/movies.js
@@ -10,4 +10,8 @@ const movieController = require('../controllers/movieController');
 // index
 router.get('/', movieController.index);
 
+// show
+router.get('/:id', movieController.show);
+
+
 module.exports = router;
