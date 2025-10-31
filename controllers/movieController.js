770a687b6dcd7eb3a6a65f4c73f3fd75362be025
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-31 14:30:47 +0100

    refactoring/fix di show()

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,21 +11,39 @@ function index(req, res) {
     });
 }
 
+
 function show(req, res) {
-    
-    const id = req.params.id
-    console.log("ID richiesto:", id); // DEBUG
-    const sql = `
-        SELECT *
-        FROM movies
-        JOIN reviews
-        ON reviews.movie_id = movies.id
-        WHERE movies.id = ?
-    `;
-    connection.query(sql, [id], (err, results) => {
-        if (err) return res.status(500).json({ error: 'Database query failed' })
-        if (results.length === 0) return res.status(404).json({ error: 'Film not found' })
-        res.json(results[0]);
+    // recuperiamo id da param
+    const id = req.params.id;
+
+    // prepariamo query per singolo film
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    // prepariamo la query per reviews del book
+    const reviewSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    // aggiungiamo la connesione per la richiesta
+    connection.query(movieSql, [id], (err, movieResult) => {
+        // gestiamo errore server mysql
+        if (err) return res.status(500).json({ error: "Database error" })
+        // gestiamo anche il 404
+        if (movieResult.length === 0) return res.status(404).json({ error: "Movie not found" })
+
+        // creiamo oggetto singolo film
+        const singleMovie= movieResult[0];
+        singleMovie.image = req.imagePath + singleMovie.image;
+
+
+        // aggiungiamo connesione per richiesta reviews relative
+        connection.query(reviewSql, [id], (err, reviewResult) => {
+            // gestiamo errore server mysql
+            if (err) return res.status(500).json({ error: "Database error" })
+            // aggiungiamo le reviews sull'oggetto del singolo film
+            singleMovie.reviews = reviewResult;
+
+            // ritorniamo il risultato ottenuto
+            res.json(singleMovie);
+        });
     });
 }
 
